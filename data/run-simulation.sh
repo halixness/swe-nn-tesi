180a13dd8d229cb3498db6b71f628bfdd3bf35d0
Diego Calanzone <diegocalanzone@gmail.com>
2020-11-19 10:00:24 +0100

    fixed processing scripts

--- data/run-simulation.sh
+++ data/run-simulation.sh
@@ -1,8 +1,11 @@
 input="input_toce_ris_V17_50.txt"
 input_aux="toce_ris_V17_50"
+timesteps=1000
 
 # Run simulazion
-./swegpu $input -order=1 -gpu=0 > "output.txt"
+./swegpu $input -order=1 -gpu=0 -frames 0 $timesteps > "output.txt"
+
+rm *-INITIAL*
 
 # Ask run recode
 read -r -p "Run the decode utility? [y/N] " response
@@ -14,11 +17,3 @@ case "$response" in
         echo ""
         ;;
 esac
-
-# Move everything
-mkdir ../output
-mv output-* ../output/
-mv *output.* ../output/
-
-# Clean
-rm "${input_aux}-INITIAL.*"
