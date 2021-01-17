4a67e2b260e328fa48719cd806553a10a7e195f8
Diego Calanzone <sc19dc@leeds.ac.uk>
2021-01-17 16:07:25 +0100

    quick fix scripts per generazione (tested)

--- 00-task/decode.sh
+++ 00-task/decode.sh
@@ -13,9 +13,9 @@ percorso="$SCRATCH/tesi/output_$2"
 nome_file_last="$nome_test-output-LAST"
 
 frame_start=0
-frame_end=$(ls $percorso/simulation | grep -v LAST | grep ".DEP" | tail -1 | awk -F'-' '{print $NF}' | awk -F'.' '{print $1}')
+frame_end=$(ls $percorso/simulation* | grep -v LAST | grep ".DEP" | tail -1 | awk -F'-' '{print $NF}' | awk -F'.' '{print $1}')
 
-swegpu=$HOME/tesi/swegpu
+swegpu=$SCRATCH/swegpu
 
 $swegpu -decode $percorso/simulation_$bcc_id/$nome_file_last.MAXWSE $percorso/decoded_$bcc_id/$nome_test-decoded-last.MAXWSE -all -binary=0 -res=$livello_ris
 $swegpu -decode $percorso/simulation_$bcc_id/$1-output-LAST.BTM $percorso/decoded_$bcc_id/$1-decoded-last.BTM -all -binary=0 -res=$livello_ris
